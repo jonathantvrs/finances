9417e92350d08254a669c45ed16d58e51cd0e944
jonathantvrs <jonathantvrs@ufrn.edu.br>
2020-07-14 15:09:25 -0300

    simplifica enums

--- src/main/java/com/jonathantvrs/minhasfinancas/enums/StatusLancamento.java
+++ src/main/java/com/jonathantvrs/minhasfinancas/enums/StatusLancamento.java
@@ -1,17 +1,7 @@
 package com.jonathantvrs.minhasfinancas.enums;
 
 public enum StatusLancamento {
-    PENDENTE("Pendente"),
-    CANCELADO("Cancelado"),
-    EFETIVADO("Efetivado");
-
-    public String statusValue;
-
-    StatusLancamento(String status) {
-        this.statusValue = status;
-    }
-
-    public String getStatusValue() {
-        return this.statusValue;
-    }
+    PENDENTE,
+    CANCELADO,
+    EFETIVADO;
 }
--- src/main/java/com/jonathantvrs/minhasfinancas/enums/TipoLancamento.java
+++ src/main/java/com/jonathantvrs/minhasfinancas/enums/TipoLancamento.java
@@ -1,16 +1,6 @@
 package com.jonathantvrs.minhasfinancas.enums;
 
 public enum TipoLancamento {
-    DESPESA("Despesa"),
-    RECEITA("Receita");
-
-    public String tipoValue;
-
-    TipoLancamento(String tipo) {
-        this.tipoValue = tipo;
-    }
-
-    public String getTipoValue() {
-        return this.tipoValue;
-    }
+    DESPESA,
+    RECEITA;
 }
