1178a16398cf8b9a61cb9a50b03e799b4565e4e9
jonathantvrs <jonathantvrs@ufrn.edu.br>
2020-07-14 15:42:49 -0300

    adiciona lombok e refatora entidades de usuario e lancamento com a annotation data

--- src/main/java/com/jonathantvrs/minhasfinancas/models/Lancamento.java
+++ src/main/java/com/jonathantvrs/minhasfinancas/models/Lancamento.java
@@ -2,6 +2,7 @@ package com.jonathantvrs.minhasfinancas.models;
 
 import com.jonathantvrs.minhasfinancas.enums.StatusLancamento;
 import com.jonathantvrs.minhasfinancas.enums.TipoLancamento;
+import lombok.Data;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import javax.persistence.Column;
@@ -17,12 +18,12 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Objects;
 
 /**
  * Classe que representa um lançamento financeiro no sistema
  */
 @Entity
+@Data
 @Table(name = "lancamento", schema = "financas")
 public class Lancamento {
     /**
@@ -86,97 +87,4 @@ public class Lancamento {
     @Column(name = "status")
     @Enumerated(value = EnumType.STRING)
     private StatusLancamento status;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public Integer getAno() {
-        return ano;
-    }
-
-    public void setAno(Integer ano) {
-        this.ano = ano;
-    }
-
-    public Integer getMes() {
-        return mes;
-    }
-
-    public void setMes(Integer mes) {
-        this.mes = mes;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
-
-    public BigDecimal getValor() {
-        return valor;
-    }
-
-    public void setValor(BigDecimal valor) {
-        this.valor = valor;
-    }
-
-    public LocalDate getDataCadastro() {
-        return dataCadastro;
-    }
-
-    public void setDataCadastro(LocalDate dataCadastro) {
-        this.dataCadastro = dataCadastro;
-    }
-
-    public TipoLancamento getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(TipoLancamento tipo) {
-        this.tipo = tipo;
-    }
-
-    public StatusLancamento getStatus() {
-        return status;
-    }
-
-    public void setStatus(StatusLancamento status) {
-        this.status = status;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Lancamento that = (Lancamento) o;
-        return Objects.equals(id, that.id) &&
-                Objects.equals(descricao, that.descricao) &&
-                Objects.equals(ano, that.ano) &&
-                Objects.equals(mes, that.mes) &&
-                Objects.equals(usuario, that.usuario) &&
-                Objects.equals(valor, that.valor) &&
-                Objects.equals(dataCadastro, that.dataCadastro) &&
-                tipo == that.tipo &&
-                status == that.status;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, descricao, ano, mes, usuario, valor, dataCadastro, tipo, status);
-    }
 }
--- src/main/java/com/jonathantvrs/minhasfinancas/models/Usuario.java
+++ src/main/java/com/jonathantvrs/minhasfinancas/models/Usuario.java
@@ -1,17 +1,19 @@
 package com.jonathantvrs.minhasfinancas.models;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.Objects;
 
 /**
  * Classe que representa um usuário no sistema
  */
 @Entity
+@Data
 @Table(name = "usuario", schema = "financas")
 public class Usuario {
 
@@ -40,52 +42,4 @@ public class Usuario {
      */
     @Column(name = "senha")
     private String senha;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Usuario usuario = (Usuario) o;
-        return Objects.equals(id, usuario.id) &&
-                Objects.equals(nome, usuario.nome) &&
-                Objects.equals(email, usuario.email) &&
-                Objects.equals(senha, usuario.senha);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, nome, email, senha);
-    }
 }
