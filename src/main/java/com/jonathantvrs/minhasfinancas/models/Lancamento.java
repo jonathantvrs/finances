f7c3155f456a56f96ac1258116c07ca0155d10d9
jonathantvrs <jonathantvrs@ufrn.edu.br>
2020-07-14 15:01:44 -0300

    mapeando entidade lancamento

--- src/main/java/com/jonathantvrs/minhasfinancas/models/Lancamento.java
+++ src/main/java/com/jonathantvrs/minhasfinancas/models/Lancamento.java
@@ -1,12 +1,150 @@
 package com.jonathantvrs.minhasfinancas.models;
 
-import java.math.BigDecimal;
+import com.jonathantvrs.minhasfinancas.enums.StatusLancamento;
+import com.jonathantvrs.minhasfinancas.enums.TipoLancamento;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "lancamento", schema = "financas")
 public class Lancamento {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "descricao")
     private String descricao;
+
+    @Column(name = "ano")
     private Integer ano;
+
+    @Column(name = "mes")
     private Integer mes;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @Column(name = "valor")
     private BigDecimal valor;
 
+    @Column(name = "data_cadastro")
+    @Convert(converter = Jsr310JpaConverters.LocalDateConverter.class)
+    private LocalDate dataCadastro;
+
+    @Column(name = "tipo")
+    @Enumerated(value = EnumType.STRING)
+    private TipoLancamento tipo;
+
+    @Column(name = "status")
+    @Enumerated(value = EnumType.STRING)
+    private StatusLancamento status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public Integer getMes() {
+        return mes;
+    }
+
+    public void setMes(Integer mes) {
+        this.mes = mes;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public TipoLancamento getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoLancamento tipo) {
+        this.tipo = tipo;
+    }
+
+    public StatusLancamento getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusLancamento status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lancamento that = (Lancamento) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(descricao, that.descricao) &&
+                Objects.equals(ano, that.ano) &&
+                Objects.equals(mes, that.mes) &&
+                Objects.equals(usuario, that.usuario) &&
+                Objects.equals(valor, that.valor) &&
+                Objects.equals(dataCadastro, that.dataCadastro) &&
+                tipo == that.tipo &&
+                status == that.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, descricao, ano, mes, usuario, valor, dataCadastro, tipo, status);
+    }
 }
