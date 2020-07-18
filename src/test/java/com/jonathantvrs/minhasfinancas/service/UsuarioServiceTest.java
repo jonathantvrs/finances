e59bdbd1320787296f587107f9750d2b1e98db36
jonathantvrs <jonathantvrs@ufrn.edu.br>
2020-07-18 15:45:56 -0300

    adicionar spring web para criar os controllers

--- src/test/java/com/jonathantvrs/minhasfinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/jonathantvrs/minhasfinancas/service/UsuarioServiceTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
