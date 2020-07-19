395c3f0f6c10254fbb4952c793431596a3252a44
jonathantvrs <jonathantvrs@ufrn.edu.br>
2020-07-18 21:51:21 -0300

    implementando service de lancamento

---
+++ src/main/java/com/jonathantvrs/minhasfinancas/service/LancamentoService.java
@@ -0,0 +1,14 @@
+package com.jonathantvrs.minhasfinancas.service;
+
+import com.jonathantvrs.minhasfinancas.enums.StatusLancamento;
+import com.jonathantvrs.minhasfinancas.models.Lancamento;
+
+import java.util.List;
+
+public interface LancamentoService {
+    Lancamento salvar(Lancamento lancamento);
+    Lancamento atualizar(Lancamento lancamento);
+    void deletar(Lancamento lancamento);
+    List<Lancamento> buscar(Lancamento lancamentoFiltro);
+    void atualizarStatus(Lancamento lancamento, StatusLancamento status);
+}
---
+++ src/main/java/com/jonathantvrs/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -0,0 +1,44 @@
+package com.jonathantvrs.minhasfinancas.service.impl;
+
+import com.jonathantvrs.minhasfinancas.enums.StatusLancamento;
+import com.jonathantvrs.minhasfinancas.models.Lancamento;
+import com.jonathantvrs.minhasfinancas.repositories.LancamentoRepository;
+import com.jonathantvrs.minhasfinancas.service.LancamentoService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LancamentoServiceImpl implements LancamentoService {
+
+    private LancamentoRepository repository;
+
+    public LancamentoServiceImpl(LancamentoRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Lancamento salvar(Lancamento lancamento) {
+        return null;
+    }
+
+    @Override
+    public Lancamento atualizar(Lancamento lancamento) {
+        return null;
+    }
+
+    @Override
+    public void deletar(Lancamento lancamento) {
+
+    }
+
+    @Override
+    public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
+        return null;
+    }
+
+    @Override
+    public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
+
+    }
+}
